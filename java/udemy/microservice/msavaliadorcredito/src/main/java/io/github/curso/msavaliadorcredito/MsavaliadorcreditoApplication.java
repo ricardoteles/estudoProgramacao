f15aa387774f7da3ace7040c24438ed6e47879b8
rteles <ricardo.teles@opus-software.com.br>
2023-01-12 10:51:18 -0300

    Criando o microservice msavaliadorcredito

---
+++ java/udemy/microservice/msavaliadorcredito/src/main/java/io/github/curso/msavaliadorcredito/MsavaliadorcreditoApplication.java
@@ -0,0 +1,13 @@
+package io.github.curso.msavaliadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsavaliadorcreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsavaliadorcreditoApplication.class, args);
+	}
+
+}
